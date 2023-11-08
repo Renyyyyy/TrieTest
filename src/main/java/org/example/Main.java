14c6ff460247f55da16e90fea30c6f62b8cdca2e
Renyyyyy <reny.zol.2019@gmail.com>
2023-11-08 17:34:01 +0300

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,32 @@
+package org.example;
+
+public class Main {
+    public static String findLongestCommonPrefix(String[] strings) {
+        Trie trie = new Trie();
+
+        for (String str : strings) {
+            trie.insert(str);
+        }
+
+        StringBuilder prefix = new StringBuilder();
+        TrieNode node = trie.getRoot();
+        while (node != null && node.children.size() == 1) {
+            char nextChar = node.children.keySet().iterator().next();
+            prefix.append(nextChar);
+            node = node.children.get(nextChar);
+        }
+
+        return prefix.toString();
+    }
+
+    public static void main(String[] args) {
+        String[] strings = {"egg","flower", "flow", "flight" };
+        String longestCommonPrefix = findLongestCommonPrefix(strings);
+        if (longestCommonPrefix.equals("")){
+            System.out.println("Самый длинный общий префикс отсутствует");
+        }else {
+            System.out.println("Самый длинный общий префикс: " + longestCommonPrefix);
+        }
+
+    }
+}
